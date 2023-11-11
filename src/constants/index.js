190126b4b776c259c785f61dc28fffabe8d412af
Shitcoder731 <shitanshu.731@gmail.com>
2023-11-11 17:06:01 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,246 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: "Web Developer",
+    company_name: "SimpleByte",
+    icon: "https://tse3.mm.bing.net/th?id=OIP.DeqMblPht0x0qifFt7KjJgAAAA&pid=Api&P=0&h=180",
+    iconBg: "#E6DEDD",
+    date: "July 2023 - August 2023",
+    points: [
+      "Developing and maintaining web applications using HTML,CSS,JAVASCRIPT and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Oasis-Infobyte",
+    icon: "https://media-exp1.licdn.com/dms/image/C4E0BAQHfk_O8amPovQ/company-logo_200_200/0/1642532597169?e=2147483647&v=beta&t=d5ptvJvSiIIZY44K2Qj9iEi_yimrIVAqxJIb8OTGNRA",
+    iconBg: "#383E56",
+    date: "September 2023 - October 2023",
+    points: [
+      "Developing and maintaining web applications using HTML,CSS,JAVASCRIPT and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+ 
+  {
+    title: "Full Stack Web Developer",
+    company_name: "Bharat-Intern",
+    icon:"https://www.iismworld.com/wp-content/uploads/2021/04/Bharat-Army-New-Logo-Black.png",
+    iconBg: "#383E56",
+    date: "September 2023 - October 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "PSSFinancials",
+    icon: "https://pssfinancial.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpss_logo.6d80ab5c.png&w=640&q=75",
+    iconBg: "#E6DEDD",
+    date: "November 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js,Typescript,TailwindCSS and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Shitanshu proved me wrong.",
+    name: "Sanji",
+    designation: "CFO",
+    company: "Ceo of Anime",
+    image: "http://images5.fanpop.com/image/photos/26300000/Sanji-one-piece-26360345-781-1024.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Shitanshu does.",
+    name: "Luffy",
+    designation: "COO",
+    company: "Manga",
+    image: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1ead1775-35e5-4321-a869-2904b619a002/d4k5xg4-43041187-e8ff-40d8-abbb-abf1f1ea935b.png/v1/fill/w_900,h_665,q_80,strp/luffy___awesome_by_cherryinthesun_d4k5xg4-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjY1IiwicGF0aCI6IlwvZlwvMWVhZDE3NzUtMzVlNS00MzIxLWE4NjktMjkwNGI2MTlhMDAyXC9kNGs1eGc0LTQzMDQxMTg3LWU4ZmYtNDBkOC1hYmJiLWFiZjFmMWVhOTM1Yi5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.Cu1WsmBKbDGktPY_8neJefUhVtn3QemnPuo6y6PINNo",
+  },
+  {
+    testimonial:
+      "It is a Good website and the clients would be impresssed by Shitanshu.",
+    name: "Zoro",
+    designation: "CTO",
+    company: "Sworsmith",
+    image: "http://images2.fanpop.com/image/photos/10400000/zoro-one-piece-10411722-2560-1848.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "DermAI",
+    description:
+      "Website that would take images of the skin cancer and detect the disease and would advise some medicines.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/Shitanshu731/DERMAI",
+  },
+  {
+    name: "Movie-Riview",
+    description:
+      "Web application that enables users to add any movie and then make the review of it by uloading the ratings.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Firebase",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Prime Video Clone",
+    description:
+      "Web application that enables the user to watch any video , series, drama with different qualities.",
+    tags: [
+      {
+        name: "HTML",
+        color: "green-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "JAVASCRIPT",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
